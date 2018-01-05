68afd68e56e6e05fcb0fdd7921620a75cd889e13
imansalehian <34875786+imansalehian@users.noreply.github.com>
2018-01-05 11:58:39 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Initial stylesheet
+console.log("Up and running!");
+//Names for cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+//Display what the user flipped
+console.log("User flipped " + cardOne);
